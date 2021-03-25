23ebe596ea01e04ab08ae23149459f62655aef75
tanaka.taro <github_kadaiyou@outlook.jp>
2021-03-25 13:38:04 +0000

    create session_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "toppages#index"
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   resources :tasks, only: [:index, :new, :create, :edit, :update] do
     member do
       post :finishment
