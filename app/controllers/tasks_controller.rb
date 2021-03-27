a4e899b53ced1fda23cac7665985b4d68ea5648a
tanaka.taro <github_kadaiyou@outlook.jp>
2021-03-27 03:15:02 +0000

    tasks/edit: also mistake

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,16 +4,24 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = current_user.tasks.build
   end
 
   def create
   end
 
   def edit
-     
+     @task = current_user.tasks.find_by(params[:id])
   end
 
   def update
+    @task = current_user.tasks.find_by(params[:id])
+    @task.content = params[:content] 
+    if @task.save
+      redirect_to "/"
+    else
+      render :edit
+    end
   end
   
   def favorite
@@ -27,4 +35,10 @@ class TasksController < ApplicationController
      @task.change_status
    end
   
+  
+  private
+
+  def task_params
+    params.require(:task).permit(:content, :favorite, :finishment)
+  end
 end
